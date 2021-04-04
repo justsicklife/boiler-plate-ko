da5785746d265b7d5c86c580160656a1a452eb8f
justsicklife <justsicklife@gmail.com>
2021-04-04 20:56:35 +0900

    Connected MongoDB

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://user:1234@boilerplate.dzzx9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+}).then(() => { console.log("MongoDb Connected... ") })
+    .catch(err => console.log(err));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
