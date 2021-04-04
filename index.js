04d95f520eca231d3390d763d19dbde986a98a33
justsicklife <justsicklife@gmail.com>
2021-04-04 23:31:07 +0900

    dotenv로 환경변수 등록

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const bodyParser = require("body-parser");
 const app = express()
-const port = 3000
+const dotenv = require("dotenv");
+dotenv.config();
+
+const port = process.env.PORT || 7000;
+
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://user:1234@boilerplate.dzzx9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -13,6 +26,21 @@ mongoose.connect("mongodb+srv://user:1234@boilerplate.dzzx9.mongodb.net/myFirstD
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다. 
+
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, error: err });
+        return res.status(200).json({
+            success: true,
+        })
+    })
+
 })
 
 app.listen(port, () => {
