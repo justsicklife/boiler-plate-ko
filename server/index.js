ed60dac697a2f3a5e60fe6b5a5918010ab7ea5a1
justsicklife <justsicklife@gmail.com>
2021-04-06 22:09:12 +0900

    set up Proxy

--- server/index.js
+++ server/index.js
@@ -108,6 +108,10 @@ app.get('/api/users/logout', auth, (req, res) => {
 
 })
 
+app.get("/api/hello",(req,res) => {
+    res.send("안녕하세요");
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
