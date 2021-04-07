78ea59ec610c4a4d7cf924d35061c3923e511c3c
justsicklife <justsicklife@gmail.com>
2021-04-07 23:15:52 +0900

    frontend login 기능 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import "antd/dist/antd.css";
 
 import React from "react";
 import {
@@ -17,22 +18,6 @@ export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">landing</Link>
-            </li>
-            <li>
-              <Link to="/login">login</Link>
-            </li>
-            <li>
-              <Link to="/register">register</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
           <Route exact path="/" component={LandingPage}/>
           <Route path="/login" component={LoginPage}/>
